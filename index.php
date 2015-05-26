adc698bd97867fda0bc50d2918f39c156646fb9c
Jon Patsenker <jp263@balcony.orchestra>
2015-05-26 17:57:46 -0400

    Added index.php file as index.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="download" action="results.php" method="POST" enctype="multipart/form-data">
+                
+            <div id="download_area">
+                
+                <p><b>FASTA</b></p>
+                <input type="file" name="fastaseq">
+                <p><b>EMAIL</b></p>
+                <textarea name="email"></textarea>
+                <p></p>
+                <input type="submit"/>
+
+
+
+
+
+            </div>
+        </form>
+        
+        
+        
+        <?php
+            
+        ?>
+    </body>
+</html>
+
