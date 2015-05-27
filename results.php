e3a7b29e364f3c7d463065773be17696883b0aa4
Jon Patsenker <jp263@balcony.orchestra>
2015-05-27 09:53:34 -0400

    Added results.php page

---
+++ results.php
@@ -0,0 +1,32 @@
+<?php
+	if(!$_POST['email']){
+        	echo '<p style="color:#FF0000">  Please fill in all fields </p>';
+    	}else{
+        	$email = $_POST['email'];
+        	$target_dir = "uploaded_fasta/";
+        	$target_file = $target_dir . basename($_FILES['fastaseq']['name']);
+        	unset($target_dir);
+        
+        	if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
+            		echo '<p style="color:#FF0000"> Bad File </p>';
+        	}
+        
+		$headers = "From: CRAP DB <noreply@kirschner.med.harvard.edu>" . "\r\n";
+        	if(mail($email, "CRAP Results", "this is your crap score: 1", $headers)){
+            		echo '<p style="color:green"> Sending email with results to ' . $email . ' </p>';
+        	}
+        	unlink($target_file);
+        	unset($target_file);
+    	}
+    
+    
+    include "index.php";
+
+
+
+
+
+
+
+
+?>
