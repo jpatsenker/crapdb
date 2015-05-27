f2fa0840aadc4a05f1d23282e23a3e19a1e20134
Jon Patsenker <jp263@balcony.orchestra>
2015-05-27 16:04:02 -0400

    Adding mining directory with scripts for data mining

---
+++ mining/calculate_redundancies.py
@@ -0,0 +1,21 @@
+def cd_hit_run(input, output, threshold):
+
+
+
+def count_non_redundant_seq(out_file):
+
+
+	return 0
+
+
+
+
+
+#Main
+
+
+//All thresholds to run CD-HIT on
+	//Don't run on anything lower than .7 or cluster may crash
+
+tholds = [.7, .8, .9]
+
