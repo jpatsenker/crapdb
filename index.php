40c636aa82b0304b50ff75ff0f610954a88bbde6
Jon Patsenker <jp263@balcony.orchestra>
2015-05-27 09:32:14 -0400

    second iter of index attempt

--- index.php
+++ index.php
@@ -1,38 +1,5 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <form name="download" action="results.php" method="POST" enctype="multipart/form-data">
-                
-            <div id="download_area">
-                
-                <p><b>FASTA</b></p>
-                <input type="file" name="fastaseq">
-                <p><b>EMAIL</b></p>
-                <textarea name="email"></textarea>
-                <p></p>
-                <input type="submit"/>
 
+test
 
-
-
-
-            </div>
-        </form>
-        
-        
-        
-        <?php
-            
-        ?>
-    </body>
 </html>
-
