30d3c8cdca3a0be3ee571e7c9a4752ee0504256d
Jon Patsenker <jonathan@patsenker.com>
2015-05-28 16:15:52 -0400

    more bug

--- mining/get_longest_transcripts.py
+++ mining/get_longest_transcripts.py
@@ -35,6 +35,7 @@ while line:
 	else:
 		print "check"
 		if not isinstance(transcript_length, numbers.Integral):
+			print "mate"
 			protein_dictionary[gene_id] = 0
 		else:
 			protein_dictionary[gene_id] = transcript_length
