93dddf34537eb83c09fa0cd4c1eaf107d4e9e974
Jon Patsenker <jonathan@patsenker.com>
2015-06-03 18:02:15 -0400

    Alpha Server Release Finish?

--- results.php
+++ results.php
@@ -13,8 +13,8 @@
             if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
                 echo '<p style="color:#FF0000"> Bad File </p>';
             }else{
-                echo 'bsub -q short -K -o' . $fixed_file . ' -e tmp/errors.txt perl ' . $fastaCheck . ' ' . $target_file . ' 0';
-                exec('bsub -q short -K -o' . $fixed_file . ' -e tmp/errors.txt perl ' . $fastaCheck . ' ' . $target_file . ' 0');
+                echo 'bsub -q short -K -W 1 -o' . $fixed_file . ' -e tmp/errors.txt perl ' . $fastaCheck . ' ' . $target_file . ' 0';
+                exec('bsub -q short -K -W 1 -o' . $fixed_file . ' -e tmp/errors.txt perl ' . $fastaCheck . ' ' . $target_file . ' 0');
                 
                 
                 
