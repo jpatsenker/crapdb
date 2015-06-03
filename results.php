d8394176c69fe0680ad8f1dfa929d8cd77c2665f
Jon Patsenker <jonathan@patsenker.com>
2015-06-03 12:31:40 -0400

    Alpha Server Release Finish?

--- results.php
+++ results.php
@@ -8,21 +8,23 @@
             $email = $_POST['email'];
             $target_dir = "uploaded_fasta/";
             $target_file = $target_dir . basename($_FILES['fastaseq']['name']);
-            unset($target_dir);
 
             if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
-                    echo '<p style="color:#FF0000"> Bad File </p>';
+                echo '<p style="color:#FF0000"> Bad File </p>';
             }else{
 
                 exec('perl ' . $fastaCheck . ' ' . $target_file . ' 0 > ' . $target_file);
-                
+                fopen($target_file, "r");
+                $stderr = fopen("php://stderr", "r");
+                if($stderr.readline()){
+                    echo '<p style="color:#FF9933"> Warning: File Was Modified by FASTA Checker</p>';
+                }
 
                 $headers = "From: CRAP DB <noreply@kirschner.med.harvard.edu>" . "\r\n";
                 if(mail($email, "CRAP Results", "this is your crap score: 1", $headers)){
-                        echo '<p style="color:green"> Sending email with results to ' . $email . ' </p>';
+                    echo '<p style="color:green"> Sending email with results to ' . $email . ' </p>';
                 }
                 unlink($target_file);
-                unset($target_file);
             }
     	}
     
