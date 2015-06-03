33a6766566901502e6507b20853a314c09373c00
Jon Patsenker <jonathan@patsenker.com>
2015-06-03 17:31:51 -0400

    Alpha Server Release Finish?

--- results.php
+++ results.php
@@ -1,6 +1,6 @@
 <?php
         $fastaCheck = '/www/kirschner.med.harvard.edu/docroot/genomes/code/fasta_checker.pl';
-
+        $score = 0;
         
 	if(!$_POST['email']){
             echo '<p style="color:#FF0000">  Please fill in all fields </p>';
@@ -16,15 +16,16 @@
                 echo 'perl ' . $fastaCheck . ' ' . $target_file . ' 0 > ' . $fixed_file . ' 2> tmp/errors.txt';
                 exec('perl ' . $fastaCheck . ' ' . $target_file . ' 0 > ' . $fixed_file . ' 2> tmp/errors.txt');
                 //exec('mv tmp/output ' . $target_file);
-                fopen($target_file, "r");
+                $fastaFile = fopen($fixed_file, "r");
                 $stderr = fopen("tmp/errors.txt", "r");
 
                 if(filesize("tmp/errors.txt") != 0){
+                    $score = '1';
                     echo '<p style="color:#FF9933"> Warning: File Was Modified by FASTA Checker</p>';
                 }
 
                 $headers = "From: CRAP DB <noreply@kirschner.med.harvard.edu>" . "\r\n";
-                if(mail($email, "CRAP Results", "this is your crap score: 1", $headers)){
+                if(mail($email, "CRAP Results", "this is your crap score: " . $score, $headers)){
                     echo '<p style="color:green"> Sending email with results to ' . $email . ' </p>';
                 }
                 //unlink($target_file);
