06eaf8f7f14cf8cd2f38aabc85b9652eb0c5d3ac
Jon Patsenker <jonathan@patsenker.com>
2015-06-05 15:27:44 -0400

    updating process_data.py

--- process_data.py
+++ process_data.py
@@ -2,6 +2,25 @@ import sys
 import io
 import os
 import subprocess
+import smtplib
+
+def send_email(info, email):
+	sender = 'noreply@kirschner.med.harvard.edu'
+	receivers = email
+
+	message = "From: CRAP DB <noreply@kirschner.med.harvard.edu> "+'\n' + "Subject: CRAP Score" + '\n' + info
+
+	try:
+		smtpObj = smtplib.SMTP('localhost')
+			smtpObj.sendmail(sender, receivers, message)
+		print "Successfully sent email"
+	except SMTPException:
+		print "Error: unable to send email"
+	#endtry
+
+#enddef
+
+
 
 
 #launch from main dir
@@ -9,21 +28,33 @@ import subprocess
 fastaChecker = '/www/kirschner.med.harvard.edu/docroot/genomes/code/fasta_checker.pl';
 
 input_file = sys.argv[1]
+mail_address = sys.argv[2]
 
 checked_file = input_file[:input_file.rfind('.')] + '_checked' + input_file[input_file.rfind('.'):]
 
 
+#MAKE LSF PROFILE FOR ORCHESTRA
+process_makeProf = subprocess.Popen('./opt/lsf/conf/profile.lsf');
 
+process_makeProf.wait()
 
 
 #PERFORM A FASTA CHECK
-process_fastaCheck = subprocess.Popen('bsub -q short -K -W 1 -o ' + fixed_file + ' -e ../tmp/errors.txt perl ' + fastaChecker + ' ' + target_file + ' 0')
+process_fastaCheck = subprocess.Popen('bsub -q short -K -W 1 -o ' + fixed_file + ' -e tmp/errors.txt perl ' + fastaChecker + ' ' + target_file + ' 0')
 
 
-process_fastaCheck.wait()
+process_fastaCheck.wait() #wait for fasta to finish before continuing
 
-fopen('tmp/errors.txt')
 
+#CHECK IF ITS OK TO CONTINUE
+with open('tmp/errors.txt') as fastaErrors:
+	if fastaErrors.readline():
+		fastaErrors.seek(0,0)
+		errorStr = fastaErrors.read()
+		send_email("Fasta file improperly formatted: \n" + errorStr, mail_address);
+		sys.exit(0);
+	#endif
+#endwith
 
 
 #ADD LENGTHS TO THE FILE
@@ -31,3 +62,10 @@ fopen('tmp/errors.txt')
 file_with_lengths = checked_file[:input_file.rfind('.')] + '_lengths' + checked_file[input_file.rfind('.'):]
 
 
+
+
+outstr = "Fasta is in proper format"
+
+
+
+send_email(outstr, mail_address)
--- results.php
+++ results.php
@@ -19,40 +19,9 @@
                 echo '<p style="color:#FF0000"> Error Moving File </p>';
             }else{
                 exec('cd mining');
-                exec('python process_data.py ' . $target_file);
+                exec('python process_data.py ' . $target_file . ' ' . $email);
                 
-                echo 'bsub -q short -K -W 1 -o ' . $fixed_file . ' -e tmp/errors.txt perl ' . $fastaCheck . ' ' . $target_file . ' 0';
-                #set up orchestra profile
-                exec('./opt/lsf/conf/profile.lsf');
-                
-                #do a fasta check - submitting job to short queue, waiting for it to end, setting 1 hour of wait time, outputing to file, and sending error to another file
-                exec('bsub -q short -K -W 1 -o ' . $fixed_file . ' -e tmp/errors.txt perl ' . $fastaCheck . ' ' . $target_file . ' 0');
-                
-                
-                
-                $fastaFile = fopen($fixed_file, "r");
-                $stderr = fopen("tmp/errors.txt", "r");
-
-                if(filesize("tmp/errors.txt") != 0){
-                    $score = 1;
-                    echo '<p style="color:red">Error Processing File: Fasta Check</p>';
-                }
-                
-                exec('bsub -q short -K -W 1 -e tmp/errors.txt python mining/add_lengths.py ' . $fixed_file . ' ' . $fixed_file_with_lengths);
-                
-                if(filesize("tmp/errors.txt") != 0){
-                    $score = 1;
-                    echo '<p style="color:red">Error Processing File: Adding Lengths</p>';
-                }
-                
-                
-                $headers = "From: CRAP DB <noreply@kirschner.med.harvard.edu>" . "\r\n";
-                if(mail($email, "CRAP Results", "this is your crap score: " . $score, $headers)){
-                    echo '<p style="color:green"> Sending email with results to ' . $email . ' </p>';
-                }
-                exec('./opt/lsf/conf/profile.lsf');
-
-                //unlink($target_file);
+                echo '<p style="color:green"> Your request is being processed. You will receive an email when your CRAP is ready" </p>';
             }
     	}
     
