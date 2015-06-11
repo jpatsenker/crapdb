66104379986c47bd3963070d4a39584463348883
Jon Patsenker <jonathan@patsenker.com>
2015-06-11 12:53:03 -0400

    Popen

--- mining/add_lengths.py
+++ mining/add_lengths.py
@@ -15,7 +15,7 @@ while line:
 	if line[0] == '>':
 		sequence = ''
 		seq_line = input.readline()
-		while sequence or seq_line[0] != '>':
+		while sequence and seq_line[0] != '>':
 			sequence += seq_line
 			seq_line = input.readline()
 			print seq_line
