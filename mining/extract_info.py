7e7603483757bbbe904d43c49a62b9b7a56db18c
Jon Patsenker <jonathan@patsenker.com>
2015-06-26 17:41:57 -0400

    complexity

---
+++ mining/extract_info.py
@@ -0,0 +1,16 @@
+import os
+
+files = os.listdir("outputs/sample/0j_out/")
+
+with open("0j_info", "w") as write:
+    for f in files:
+        if f != "empty_file_for_git":
+            with open(f, "r") as stream:
+                info = stream.read()
+                try:
+                    val = int(info[info.find(".95,")+4:info.find("-",info.find(".95,"))])
+                    write.write(f[0:5] + ": " + val)
+                except ValueError:
+                    print "NO!"
+                    exit(1)
+            print "File " + f + "\n"
