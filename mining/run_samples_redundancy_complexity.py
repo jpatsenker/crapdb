3fc380dd6004d695e71ea5b8196755a35b967df0
Jon Patsenker <jonathan@patsenker.com>
2015-06-26 16:28:09 -0400

    complexity

--- mining/run_samples_redundancy_complexity.py
+++ mining/run_samples_redundancy_complexity.py
@@ -6,7 +6,7 @@ files = os.listdir("inputs/sample/")
 for f in files:
     if f != "empty_file_for_git":
         process_f_0j = subprocess.Popen(['/bin/bash', '-c',
-                                         '../run_with_profile.sh -q short -o /dev/null -K -W 12 python calculate_complexities.py inputs/sample/' + f + ' outputs/0j_out/sample/' + f + '.0j'])
+                                         '../run_with_profile.sh -q short -o /dev/null -W 12 python calculate_complexities.py inputs/sample/' + f + ' outputs/0j_out/sample/' + f + '.0j'])
         process_f_cdhit = subprocess.Popen(['/bin/bash', '-c',
-                                            '../run_with_profile.sh -q short -o /dev/null -K -W 12 python calculate_redundancies.py inputs/sample/' + f + ' outputs/cdhit_out/sample/' + f + '.cdhit'])
+                                            '../run_with_profile.sh -q short -o /dev/null -W 12 python calculate_redundancies.py inputs/sample/' + f + ' outputs/cdhit_out/sample/' + f + '.cdhit'])
         print "Finished file " + f + "\n"
