1ae9cf38f7045b186836ebc238186912a59d279a
Jon Patsenker <jonathan@patsenker.com>
2015-06-26 17:42:55 -0400

    complexity

--- mining/extract_info.py
+++ mining/extract_info.py
@@ -5,7 +5,7 @@ files = os.listdir("outputs/0j_out/sample")
 with open("0j_info", "w") as write:
     for f in files:
         if f != "empty_file_for_git":
-            with open(f, "r") as stream:
+            with open("outputs/0j_out/sample" + f, "r") as stream:
                 info = stream.read()
                 try:
                     val = int(info[info.find(".95,")+4:info.find("-",info.find(".95,"))])
