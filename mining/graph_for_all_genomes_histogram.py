8137278c2d5c2bcff96f7b3d1b3ce1096879dd2f
Jon Patsenker <jonathan@patsenker.com>
2015-07-27 13:40:39 -0400

    stuff

---
+++ mining/graph_for_all_genomes_histogram.py
@@ -0,0 +1,96 @@
+import sys
+import os
+import matplotlib
+import bisect
+
+matplotlib.use('Agg')
+
+import numpy as np
+import pylab as pl
+
+poi = 0
+direc = ""
+outfile = ""
+files = []
+
+try:
+    direc = sys.argv[1]
+except IndexError:
+    print "Improper Parameter: missing directory"
+    exit(1)
+
+try:
+    files = os.listdir(direc)
+except OSError:
+    print "Improper Parameter: directory does not exist"
+    exit(1)
+
+try:
+    outfile = sys.argv[2]
+except IndexError:
+    print "Improper Parameter: missing out-file"
+    exit(1)
+
+try:
+    poi = sys.argv[3]
+except IndexError:
+    print "Improper Parameter: missing % of interest"
+    exit(1)
+
+try:
+    poi = float(poi)
+except ValueError:
+    print "Improper Parameter: % of interest must be a float"
+    exit(1)
+
+d = {}
+
+for f in files:
+    if f[f.rfind("."):] == ".0j" or f[f.rfind("."):] == ".cdhit":
+        with open(direc + f) as stream_f:
+            everything = stream_f.read()
+            pairs = everything.split("\n")
+            pairs.pop() #rid of last empty pair
+            for pair in pairs:
+                ord_pair = pair.split(",")
+                try:
+                    if float(ord_pair[0]) == poi:
+                        try:
+                            d[float(ord_pair[1])] = d[float(ord_pair[1])] + 1
+                        except IndexError:
+                            d[float(ord_pair[1])] = 1
+                        print str(f) + " " + str(d[f]) + "\n"
+                except (IndexError, ValueError):
+                    print "Improperly Formatted File"
+                    exit(1)
+
+
+xs = d.keys()
+ys = d.values()
+
+X = np.arange(len(xs))
+pl.bar(X, ys, align = 'edge')
+pl.xticks(X, xs, rotation='vertical')
+
+try:
+    pl.ylim(float(sys.argv[4]),float(sys.argv[5]))
+except ValueError:
+    print "Improper limit"
+except IndexError:
+    try:
+        pl.ylim(0,float(sys.argv[4]))
+    except IndexError:
+        pl.ylim(0,1)
+
+fig = pl.gcf()
+#axis.set_title( "" )
+if files[0][files[0].find(".")+1:] == "cdhit":
+    pl.xlabel( "Threshold (%)" )
+    pl.ylabel( "Fraction Clusters to Total Sequences" )
+else:
+    pl.xlabel( "% Complexity" )
+    pl.ylabel( "% of Corpus" )
+
+fig.tight_layout()
+pl.savefig(outfile)
+pl.show()
