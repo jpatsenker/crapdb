1ebe2f9e9f2b6ce39617ba0c32ad550386572eca
Jon Patsenker <jonathan@patsenker.com>
2015-07-28 12:32:51 -0400

    stuff

--- mining/graph_for_all_genomes_histogram.py
+++ mining/graph_for_all_genomes_histogram.py
@@ -8,6 +8,18 @@ matplotlib.use('Agg')
 import numpy as np
 import pylab as pl
 
+def perform_bin(range_list, num):
+    beg = bisect.bisect_right(range_list, num)
+    if range_list[beg - 1] == num:  # Handle Perfect Hit Edge Case
+        return [num, num]
+    elif not beg:  # Left Edge Case
+        return [None, range_list[0]]
+    elif beg == len(range_list):  # Right Edge Case
+        return [range_list[-1], None]
+    else:
+        return range_list[beg - 1:beg + 1]
+
+
 poi = 0
 direc = ""
 outfile = ""
@@ -45,24 +57,32 @@ except ValueError:
 
 d = {}
 
+bins = [0,.01,.02,.03,.04,.05,.06,1]
+
+
+
 for f in files:
     if f[f.rfind("."):] == ".0j" or f[f.rfind("."):] == ".cdhit":
         with open(direc + f) as stream_f:
             everything = stream_f.read()
             pairs = everything.split("\n")
             pairs.pop() #rid of last empty pair
+            cumulative = 0
             for pair in pairs:
                 print pair + "\n"
                 ord_pair = pair.split(",")
                 try:
+                    if float(ord_pair[0]) <= poi:
+                        cumulative += float(ord_pair[1])
                     if float(ord_pair[0]) == poi:
                         try:
-                            d[float(ord_pair[0])] = d[float(ord_pair[1])] + 1
+                            d[perform_bin(bins, cumulative)[0]] = d[perform_bin(bins, cumulative)[0]] + 1
                         except KeyError:
-                            d[float(ord_pair[0])] = 1
+                            d[perform_bin(bins, cumulative)[0]] = 1
                 except (IndexError, ValueError):
                     print "Improperly Formatted File"
                     exit(1)
+
 
 
 xs = d.keys()
