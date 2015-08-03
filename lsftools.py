469e3c7ea654ce8dc099cb0561a991214829030d
Jon Patsenker <jonathan@patsenker.com>
2015-08-03 12:30:57 -0400

    stuff

--- lsftools.py
+++ lsftools.py
@@ -1,6 +1,6 @@
 import subprocess
 def run_job(command, output="/dev/null", queue = "short", timelim = 1, wait = False, return_process = False):
-    a = subprocess.Popen(["/bin/bash", "-c" ,"./run_with_profile.sh -q " + queue + " -W " + str(timelim) + " -o " + output + " " + command])
+    a = subprocess.Popen(["/bin/bash", "-c" ,"./run_with_profile.sh -q " + queue + " -K -W " + str(timelim) + " -o " + output + " " + command])
     if wait:
         a.wait()
     if return_process:
