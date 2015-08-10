03561784f4373a06a1ba5ef8d8ca12e41a9303ee
Jon Patsenker <jonathan@patsenker.com>
2015-08-10 16:46:46 -0400

    stuff

--- process_crap.py
+++ process_crap.py
@@ -5,6 +5,11 @@ ss = SewageSystem()
 
 len_filter = SeqLengthFilter(30, 30000)
 
+iFile = None
+oFile = None
+dFile = None
+tDir = None
+
 ss.add_filter(len_filter)
 
-ss.filter_crap()
+ss.filter_crap(iFile, oFile, dFile, tDir)
