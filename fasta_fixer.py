484fec569cd00641646003ab9ab90cc83f216a9e
Jon Patsenker <jonathan@patsenker.com>
2015-08-11 14:11:00 -0400

    stuff

--- fasta_fixer.py
+++ fasta_fixer.py
@@ -1,9 +1,11 @@
+import shutil
 def fix_file(input_file):
     with open(input_file, "r") as ifile:
         everything = ifile.read()
-    with open(input_file, "w") as ofile:
+    with open(input_file + ".tmp", "w") as ofile:
         for line in everything.split("\n"):
             if line[0] == ">":
                 ofile.write("\n" + line + "\n")
             else:
                 ofile.write(line)
+    shutil.move(input_file + ".tmp", input_file)
