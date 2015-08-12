065cd8b49a75dc1bebde41e664bdc96a106d249b
Jon Patsenker <jonathan@patsenker.com>
2015-08-12 18:02:58 -0400

    stuff

--- results.php
+++ results.php
@@ -38,11 +38,11 @@
 
                 mail($email, "CRAP REQUEST SENT", "We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes", 'From: "CRAP DB" <noreply@kirschner.med.harvard.edu>');
                 
-                echo "We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes";
+                echo "<p> We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes </p>";
                 #echo 'python process_crap.py ' . $target_file . ' ' . $target_file . '.clean ' . $target_file . '.messy ' . $email . ' > /dev/null 2>&1 &';
                 exec('python process_crap.py ' . $target_file . ' ' . $target_file . '.clean.txt ' . $target_file . '.messy.txt ' . $email . ' > /dev/null 2>&1 &');
                 
-                echo 'You will receive an email when your CRAP is ready. <br>';
+                echo '<p> You will receive an email when your CRAP is ready. </p>';
                 echo '<p><a href="logs/' . $next_id . '.log"> Log file for job </a></p>';
             }
             
