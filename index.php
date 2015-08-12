c32719d37553013b92b6c725fe8baadb29a80878
Jon Patsenker <jonathan@patsenker.com>
2015-08-12 16:24:08 -0400

    stuff

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <div style="width:100%;">
             <table>
                 <tr>
-                    <td><div style="width:30%;"></div></td>
+                    <td width="30%"><div style="width:30%;"></div></td>
                     <td>
                         <div class="bigblock">
                             <div class="headline"> crap web server</div>
@@ -32,7 +32,7 @@
                             </form>
                         </div>
                     </td>
-                    <td><div style="width:30%;"></div></td>
+                    <td width="30%"><div style="width:30%;"></div></td>
             </table>
         </div>
     </body>
