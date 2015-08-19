c123e9867bd0d527e0504c97501c76cf9c7be900
Jon Patsenker <jonathan@patsenker.com>
2015-08-19 17:42:52 -0400

    stuff

--- results.php
+++ results.php
@@ -37,6 +37,7 @@
             echo '<p>  Please fill in all fields </p>';
     	}else{
             #get information
+            print $_POST;
             $email = $_POST['email'];
             $ct = $_POST['ct'];
             $cl = $_POST['cl'];
@@ -74,7 +75,7 @@
                 mail($email, "CRAP REQUEST SENT", "We are processing your file as: " . $target_file . " size: " . filesize($target_file) . ' bytes.<br> ' . $fullpath . '<br>', $headers);
                 
                 echo "<p> We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes </p>";
-                #echo 'python process_crap.py ' . $target_file . ' ' . $target_file . '.clean ' . $target_file . '.messy ' . $email . ' > /dev/null 2>&1 &';
+                echo 'python process_crap_temp.py ' . $target_file . ' ' . $target_file . '.clean.txt ' . $target_file . '.messy.txt ' . $email . '-ct' . $ct . '-cl' . $cl . '-0j' . $zj . '-min' . $min . '-max' . $max . $compl . ' > /dev/null 2>&1 &';
                 exec('python process_crap_temp.py ' . $target_file . ' ' . $target_file . '.clean.txt ' . $target_file . '.messy.txt ' . $email . '-ct' . $ct . '-cl' . $cl . '-0j' . $zj . '-min' . $min . '-max' . $max . $compl . ' > /dev/null 2>&1 &');
                 
                 echo '<p> You will receive an email when your CRAP is ready. </p>';
