cefff376d0191578aee815925de135d13eb0fcb2
Jon Patsenker <jonathan@patsenker.com>
2015-08-19 19:44:05 -0400

    stuff

--- process_crap_temp.py
+++ process_crap_temp.py
@@ -99,10 +99,10 @@ ss.add_module(num_seq_bef_anlzr) #check before
 
 #FILTERS
 ss.add_module(fasta_filter)
-ss.add_module(simple_filter)
-ss.add_module(len_filter)
-#ss.add_module(comp_filter)
-ss.add_module(red_filter)
+#ss.add_module(simple_filter)
+#ss.add_module(len_filter)
+ss.add_module(comp_filter)
+#ss.add_module(red_filter)
 
 ss.add_module(num_seq_aft_anlzr) #check after
 
