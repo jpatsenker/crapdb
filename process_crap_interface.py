30deb5c968effe8c1247313efadfb5cce8a498db
Jon Patsenker <jonathan@patsenker.com>
2015-08-25 23:40:32 -0400

    stuff

--- process_crap_interface.py
+++ process_crap_interface.py
@@ -1,5 +1,6 @@
 import lsftools
 import sys
+import os
 
 command_string = "python process_crap.py"
 
@@ -7,6 +8,6 @@ for a in sys.argv[1:]:
     command_string += " " + a
 
 print command_string + "\n"
-lsftools.run_job(command_string, wait=True, dont_clean=True, bsub_output="stdout", bsub_error="stderr")
+lsftools.run_job(command_string, wait=True, dont_clean=True, bsub_output=os.ttyname(sys.stdout.fileno()), bsub_error=os.ttyname(sys.stderr.fileno()))
 
 print "done"
