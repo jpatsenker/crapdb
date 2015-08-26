829e78664a9eac5f9b143a367b631d03458abb59
Jon Patsenker <jonathan@patsenker.com>
2015-08-25 23:20:00 -0400

    stuff

---
+++ process_crap_interface.py
@@ -0,0 +1,11 @@
+import lsftools
+import sys
+
+command_string = "python process_crap.py"
+
+for a in sys.argv[1:]:
+    command_string += " " + a
+
+lsftools.run_job(command_string, wait=True)
+
+print "done"
