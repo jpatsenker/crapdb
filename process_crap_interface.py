3e94c4dbe1c4e7ee093806d47765a50f8fde1d30
Jon Patsenker <jonathan@patsenker.com>
2015-08-25 23:20:29 -0400

    stuff

--- process_crap_interface.py
+++ process_crap_interface.py
@@ -6,6 +6,7 @@ command_string = "python process_crap.py"
 for a in sys.argv[1:]:
     command_string += " " + a
 
+print command_string + "\n"
 lsftools.run_job(command_string, wait=True)
 
 print "done"
