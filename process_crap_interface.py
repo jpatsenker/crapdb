9edd25b8febb67529a39eae10aa1bdb3ff2c2678
Jon Patsenker <jonathan@patsenker.com>
2015-08-25 23:59:38 -0400

    stuff

--- process_crap_interface.py
+++ process_crap_interface.py
@@ -8,6 +8,6 @@ for a in sys.argv[1:]:
     command_string += " " + a
 
 print command_string + "\n"
-lsftools.run_job(command_string, wait=True, dont_clean=True, bsub_output="tmp/tmp_superlog", bsub_error="tmp/tmp_superlog_err")
+lsftools.run_job(command_string, wait=True, dont_clean=True, bsub_output="/dev/null", bsub_error="/dev/null")
 
 print "done"
