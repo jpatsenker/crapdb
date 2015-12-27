3c61645676eddebc9df0527d923fe4f0a02afa5a
Jon Patsenker <jonathan@patsenker.com>
2015-12-27 17:52:29 -0500

    fix cdhit hash agian

--- cdhit_filter.py
+++ cdhit_filter.py
@@ -106,8 +106,8 @@ class RedundancyFilter(SewageFilter):
         try:
             position = self.__temp_hash__[prot]
         except KeyError:
-            print "Improperly put together hash in CDHIT filter!!! Couldn't find start" + prot + "end"
-            print "From line: " + cdhitline
+            # print "Improperly put together hash in CDHIT filter!!! Couldn't find start" + prot + "end"
+            # print "From line: " + cdhitline
             exit(1)
 
         in_stream.seek(position)
