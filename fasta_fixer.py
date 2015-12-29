d7f45af27e99eb7f5e4b25164320c0437fafbdcc
Jon Patsenker <jonathan@patsenker.com>
2015-12-29 18:50:03 -0500

    fix cdhit hash agian

--- fasta_fixer.py
+++ fasta_fixer.py
@@ -6,7 +6,7 @@ def fix_file(input_file, output_file):
         for datum in everything.split(">"):
             ofile.write(">")
             seq_start = datum.find("\n")
-            while datum[seq_start].isspace():
+            while seq_start<len(datum) and datum[seq_start].isspace():
                 seq_start+=1
             seqTmp = datum[seq_start+1:]
             seq = "".join(seqTmp.split())
