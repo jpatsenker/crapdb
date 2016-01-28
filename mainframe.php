468c8b9593ac4ccb93fdd6b97dc1d7237a761b31
Jon Patsenker <jonathan@patsenker.com>
2016-01-28 10:52:45 -0500

    fixed web

--- mainframe.php
+++ mainframe.php
@@ -12,22 +12,40 @@
                                 <br><b>FASTA: </b> <input type="file" name="fastaseq"><p>
                                 <b>EMAIL: </b> <input type="text" name="email"></input>
                                 <p><b>PARAMETERS</b></p>
-                                CD-HIT Threshold: 
-                                <input type="text" name="ct" value="0.7"></input><br>
-                                CD-HIT Fractional Length:
-                                <input type="text" name="cl" value="0.8"></input><br>
-                                0j Minimum Complexity: 
-                                <input type="text" name="zj" value="0.9"></input><br>
-                                Minimum Length: 
-                                <input type="text" name="min" value="30"></input><br>
-                                Maximum Length: 
-                                <input type="text" name="max" value="30000"></input><br>
-                                Maximum Number of "X"s to be ignored:
-                                <input type="text" name="xs" value="0"></input><br>
-                                Fusion/Fission Threshold:
-                                <input type="text" name="fft" value="0.7"></input><br>
-                                Fusion/Fission Fractional Length:
-                                <input type="text" name="ffl" value="0.8"></input><p>
+                                <table>
+                                    <tr>
+                                        <td>CD-HIT Threshold:</td>
+                                        <td><input type="text" name="ct" value="0.7"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>CD-HIT Fractional Length:</td>
+                                        <input type="text" name="cl" value="0.8"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>0j Minimum Complexity: </td>
+                                        <input type="text" name="zj" value="0.9"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Minimum Length: </td>
+                                        <td><input type="text" name="min" value="30"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Maximum Length: </td>
+                                        <td><input type="text" name="max" value="30000"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Maximum Number of "X"s to be ignored:</td>
+                                        <td><input type="text" name="xs" value="0"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Fusion/Fission Threshold:</td>
+                                        <td><input type="text" name="fft" value="0.7"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Fusion/Fission Fractional Length:</td>
+                                        <td><input type="text" name="ffl" value="0.8"></input></td>
+                                    </tr>
+                                </table>
                                 <input type="checkbox" name="ms"> Check for M at beginning of sequence? <br>
                                 <input type="checkbox" name="dlen"> Destage Length Filter? <br>
                                 <input type="checkbox" name="dcomp"> Destage Complexity (0j) Filter? <br>
