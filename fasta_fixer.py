890d46f3e7838fa73525aca4e17e5acbd4974964
Jon Patsenker <jonathan@patsenker.com>
2016-01-28 17:11:51 -0500

    test

--- fasta_fixer.py
+++ fasta_fixer.py
@@ -30,8 +30,6 @@ def fix_file(input_file, output_file):
                 if datum == "":
                     break;
 
-                print datum
-
                 if datum.isspace() or len(datum) == 0:
                     continue
                 ofile.write(">")
