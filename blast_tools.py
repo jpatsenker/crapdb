5d2a672a4a7d9ca741c6ce2f819a3357c70c7ca6
Jon Patsenker <jonathan@patsenker.com>
2016-06-03 18:14:48 -0400

    New Fusion Fission

--- blast_tools.py
+++ blast_tools.py
@@ -5,7 +5,7 @@ def runBlast(sequences, reference, output):
 	loadBlast()
 	name = random.random()
 	out = random.random()
-	makedb = subprocess.Popen("makeblastdb -in " + reference + " -dbtype 'prot' -out " + out + " -name -" + name)
+	makedb = subprocess.Popen("makeblastdb -in " + reference + " -dbtype 'prot' -out " + out + " -name " + name)
 	makedb.wait()
 	run = subprocess.Popen("blastp -outfmt 8 -evalue 1e-5 -db " + name + " -query " + sequences + " -out " + output)
 	run.wait()
