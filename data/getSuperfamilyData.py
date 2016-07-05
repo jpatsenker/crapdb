435ddc4bc8f6c59aed5223f54a095e453d22119d
Jon Patsenker <jonathan@patsenker.com>
2016-07-05 15:49:42 -0400

    python stuff

--- data/getSuperfamilyData.py
+++ data/getSuperfamilyData.py
@@ -1,12 +1,22 @@
 import string
 
+
+class Genome:
+	def __init__(self, name, )
+
+
+
+header = None
+
+#convert into csv and get header
 with open("superfamily/genomes", "r") as reader:
 	line1 = reader.readline()
 	line2 = reader.readline()
 	line3 = reader.readline()
 
-	line2Info = map(string.lstrip, map(string.rstrip, line2.split('|')))[1:-1]
-	assert line2Info == ['genome', 'name', 'include', 'excuse', 'domain', 'comment', 'taxonomy', 'taxon_id', 'download_link', 'download_date', 'gene_link', 'homepage', 'password', 'parse', 'order1', 'supfam', 'order2']
+	header = map(string.lstrip, map(string.rstrip, line2.split('|')))[1:-1]
+	assert header == ['genome', 'name', 'include', 'excuse', 'domain', 'comment', 'taxonomy', 'taxon_id', 'download_link', 'download_date', 'gene_link', 'homepage', 'password', 'parse', 'order1', 'supfam', 'order2']
 
 	with open("superfamily/genomeInfo.csv", "w") as writer:
-		map(lambda line: writer.write(str(reduce(lambda x, y: x+","+y, map(string.lstrip, map(string.rstrip, line.split('|')))[1:-1], "") + "\n")[1:]), reader)
+		map(lambda line: writer.write(str(reduce(lambda x, y: x+" "+y, map(string.lstrip, map(string.rstrip, line.split('|')))[1:-1], "") + "\n")[1:]), reader)
+
