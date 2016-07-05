80ebc4588861e7e9d06eff7b2fb167732cbfbef5
Jon Patsenker <jonathan@patsenker.com>
2016-07-05 15:23:11 -0400

    python stuff

--- data/getSuperfamilyData.py
+++ data/getSuperfamilyData.py
@@ -9,4 +9,4 @@ with open("superfamily/genomes", "r") as reader:
 	assert line2Info == ['genome', 'name', 'include', 'excuse', 'domain', 'comment', 'taxonomy', 'taxon_id', 'download_link', 'download_date', 'gene_link', 'homepage', 'password', 'parse', 'order1', 'supfam', 'order2']
 
 	with open("superfamily/genomeInfo.csv", "w") as writer:
-		map(lambda line: writer.write((reduce(lambda x, y: x+","+y, map(string.lstrip, map(string.rstrip, line.split('|')))[1:-1], "") + "\n")[1:], reader)
+		map(lambda line: writer.write(str(reduce(lambda x, y: x+","+y, map(string.lstrip, map(string.rstrip, line.split('|')))[1:-1], "") + "\n")[1:], reader)
