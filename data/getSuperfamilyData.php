95271f2684ce7ab090e694d49168c3b90e40dba2
Jon Patsenker <jonathan@patsenker.com>
2016-07-05 12:07:54 -0400

    phpmysql

--- data/getSuperfamilyData.php
+++ data/getSuperfamilyData.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "localhost";
 	$username = "jp263";
-	$password = "ronda,1n,ropesM";
+	$password = "";
 
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password);
