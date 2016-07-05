bf2d9019016195ae2c062d52f3f4549911496149
Jon Patsenker <jonathan@patsenker.com>
2016-07-05 12:02:38 -0400

    phpmysql

---
+++ data/getSuperfamilyData.php
@@ -0,0 +1,14 @@
+<?php
+	$servername = "devkirschner_superfamily";
+	$username = "jp263";
+	$password = "ronda,1n,ropesM";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password);
+
+	// Check connection
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
+	echo "Connected successfully";
+?>
