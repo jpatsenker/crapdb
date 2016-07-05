b1a53f9cff578859e11c8dcc9bed3fc4121345ec
Jon Patsenker <jonathan@patsenker.com>
2016-07-05 16:01:54 -0400

    python stuff

--- data/getSuperfamilyData.py
+++ data/getSuperfamilyData.py
@@ -11,6 +11,6 @@ with open("superfamily/genomes", "r") as reader:
 	header = map(string.lstrip, map(string.rstrip, line2.split('|')))[1:-1]
 	assert header == ['genome', 'name', 'include', 'excuse', 'domain', 'comment', 'taxonomy', 'taxon_id', 'download_link', 'download_date', 'gene_link', 'homepage', 'password', 'parse', 'order1', 'supfam', 'order2']
 
-	with open("superfamily/genomeInfo.tsv", "w") as writer:
-		map(lambda line: writer.write(str(reduce(lambda x, y: x+'\t'+y, map(string.lstrip, map(string.rstrip, line.split('|')))[1:-1], "") + "\n")[1:]), reader)
+	with open("superfamily/genomeInfo.csv", "w") as writer:
+		map(lambda line: writer.write(str(reduce(lambda x, y: x+','+y, map(string.lstrip, map(string.rstrip, line.split('|')))[1:-1], "") + "\n")[1:]), reader)
 
