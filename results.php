71934c49f86cf54ff6056e24ae46c17de061f566
Jon Patsenker <jonathan@patsenker.com>
2016-07-15 15:52:11 -0400

    tst

--- results.php
+++ results.php
@@ -89,6 +89,8 @@
             if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
                 echo '<div class="notout">';
                 echo 'Error Moving File <br>';
+                echo '<form action="index.php"><input type="submit" value="Back"></form>'
+
             }else{
                 echo '<div class="outputs">';
 
@@ -106,13 +108,13 @@
                 
                 echo '<p> You will receive an email when the results are ready. </p>';
                 echo '<p><a href="logs/' . $next_id . '.log"> Log file for job </a></p>';
+                echo '<form action="index.php"><input type="submit" value="Back"></form>'
+
             }
             
     	}
     
     echo "</div></td></tr></table>";
-
-    echo '<form action="index.php"><input type="submit" value="Back"></form>'
 
 ?>
 
