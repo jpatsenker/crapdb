00f85c9406f4a5d383d2e02b95a4fff86dd8066b
Jon Patsenker <jonathan@patsenker.com>
2016-07-15 15:54:27 -0400

    tst

--- results.php
+++ results.php
@@ -35,85 +35,85 @@
             echo '<div class="notout">';
 
             echo '<p>  Please fill in all fields </p>';
-    	}else{
-            #get information
-            $email = $_POST['email'];
-            $ct = $_POST['ct'];
-            $cl = $_POST['cl'];
-            $min = $_POST['min'];
-            $max = $_POST['max'];
-            $dComp = $_POST['dcomp'];
-            $dRed = $_POST['dred'];
-            $dLen = $_POST['dlen'];
-            $dFf = $_POST['dff'];
-            $zj = $_POST['zj'];
-            $xs = $_POST['xs'];
-            $ms = $_POST['ms'];
-            $fft = $_POST['fft'];
-            $ffl = $_POST['ffl'];
-            
-            if ($dLen == "on"){
-                $dLen = " -nolen ";
-            }else{
-                $dLen = "";
-            }
-            if ($dRed == "on"){
-                $dRed = " -nored ";
-            }else{
-                $dRed = "";
-            }
-            if ($dComp -= "on"){
-                $dComp = " -nocomp ";
-            }else{
-                $dComp = "";
-            }
-            if ($dFf == "on"){
-                $dFf = " -noff ";
-            }else{
-                $dFf = "";
-            }
-            if ($ms == "on"){
-                $ms = " -ms ";
-            }else{
-                $ms = "";
-            }
-            
-            
-            $target_dir = "uploaded_fasta/";
-            $next_id = get_next_id();
-            $target_file = $target_dir . $next_id;
-                     
-            
-            
-            #move file into uploaded folder
-            if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
-                echo '<div class="notout">';
-                echo 'Error Moving File <br>';
-                echo '<form action="index.php"><input type="submit" value="Back"></form>';
+	}else{
+        #get information
+        $email = $_POST['email'];
+        $ct = $_POST['ct'];
+        $cl = $_POST['cl'];
+        $min = $_POST['min'];
+        $max = $_POST['max'];
+        $dComp = $_POST['dcomp'];
+        $dRed = $_POST['dred'];
+        $dLen = $_POST['dlen'];
+        $dFf = $_POST['dff'];
+        $zj = $_POST['zj'];
+        $xs = $_POST['xs'];
+        $ms = $_POST['ms'];
+        $fft = $_POST['fft'];
+        $ffl = $_POST['ffl'];
+        
+        if ($dLen == "on"){
+            $dLen = " -nolen ";
+        }else{
+            $dLen = "";
+        }
+        if ($dRed == "on"){
+            $dRed = " -nored ";
+        }else{
+            $dRed = "";
+        }
+        if ($dComp -= "on"){
+            $dComp = " -nocomp ";
+        }else{
+            $dComp = "";
+        }
+        if ($dFf == "on"){
+            $dFf = " -noff ";
+        }else{
+            $dFf = "";
+        }
+        if ($ms == "on"){
+            $ms = " -ms ";
+        }else{
+            $ms = "";
+        }
+        
+        
+        $target_dir = "uploaded_fasta/";
+        $next_id = get_next_id();
+        $target_file = $target_dir . $next_id;
+                 
+        
+        
+        #move file into uploaded folder
+        if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
+            echo '<div class="notout">';
+            echo 'Error Moving File <br>';
 
-            }else{
-                echo '<div class="outputs">';
+        }else{
+            echo '<div class="outputs">';
 
-                $headers = 'From: "CRAP Pipeline" <noreply@kirschner.med.harvard.edu>\r\n';
-                $headers .= "MIME-Version: 1.0\r\n";
-                $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+            $headers = 'From: "CRAP Pipeline" <noreply@kirschner.med.harvard.edu>\r\n';
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-                $fullpath = substr(getcwd(),strpos(getcwd(), "/www/") + 5) . "/logs/" . $next_id . ".log";
-                $fullpath = str_replace("/docroot/", "/", $fullpath);
-                mail($email, "CRAP REQUEST SENT", "We are processing your file as: " . $target_file . " size: " . filesize($target_file) . ' bytes.<br> ' . $fullpath . '<br>', $headers);
-                
-                echo "<p> We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes </p>";
-                echo 'python process_crap_interface.py ' . $target_file . ' ' . $target_file . '.clean.txt ' . $target_file . '.messy.txt ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed . $dFf .' > superlog 2>&1 &';
-                exec('python process_crap_interface.py ' . $target_file . ' ' . $target_file . '.clean.txt ' . $target_file . '.messy.txt ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed . $dFf .' > superlog 2>&1 &');
-                
-                echo '<p> You will receive an email when the results are ready. </p>';
-                echo '<p><a href="logs/' . $next_id . '.log"> Log file for job </a></p>';
-                echo '<form action="index.php"><input type="submit" value="Back"></form>';
-
-            }
+            $fullpath = substr(getcwd(),strpos(getcwd(), "/www/") + 5) . "/logs/" . $next_id . ".log";
+            $fullpath = str_replace("/docroot/", "/", $fullpath);
+            mail($email, "CRAP REQUEST SENT", "We are processing your file as: " . $target_file . " size: " . filesize($target_file) . ' bytes.<br> ' . $fullpath . '<br>', $headers);
             
-    	}
-    
+            echo "<p> We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes </p>";
+            echo 'python process_crap_interface.py ' . $target_file . ' ' . $target_file . '.clean.txt ' . $target_file . '.messy.txt ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed . $dFf .' > superlog 2>&1 &';
+            exec('python process_crap_interface.py ' . $target_file . ' ' . $target_file . '.clean.txt ' . $target_file . '.messy.txt ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed . $dFf .' > superlog 2>&1 &');
+            
+            echo '<p> You will receive an email when the results are ready. </p>';
+            echo '<p><a href="logs/' . $next_id . '.log"> Log file for job </a></p>';
+
+        }
+            
+    }
+
+    echo '<form action="index.php"><input type="submit" value="Back"></form>';
+
     echo "</div></td></tr></table>";
 
 ?>
