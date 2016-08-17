6a4fda3e6c3e70e2063971655f3d3fa2eaa94034
Jon Patsenker <jonathan@patsenker.com>
2016-08-17 16:32:05 -0400

    fix

--- tests/testWebLoadModule.php
+++ tests/testWebLoadModule.php
@@ -3,15 +3,15 @@
 	echo "<HTML><HEAD><TITLE>TEST</TITLE><HEAD><BODY>";
 
 	echo "Initial test phmmer (expect error) <br>";
-	exec 'phmmer > superlog 2>&1 &';
+	#exec 'phmmer > superlog 2>&1 &';
 	echo "PHMMER Complete <p>";
 
 	echo "Testing Load Module of seq/hmmer/3.1 <br>";
-	#exec 'module load seq/hmmer/3.1 > superlog';
+	exec 'module load seq/hmmer/3.1 > superlog 2>&1 &';
 	echo "Load Module Complete <p>";
 
 	echo "Testing phmmer (expect usage explanation) <br>";
-	#exec 'phmmer > superlog';
+	#exec 'phmmer > superlog 2>&1 &';
 	echo "PHMMER Complete <p>";
 
 	echo "</BODY></HTML>";
