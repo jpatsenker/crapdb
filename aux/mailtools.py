908b05d226b9b7a8692cad6a4d35a24f8bf50d75
Jon Patsenker <jonathan@patsenker.com>
2018-12-19 21:38:28 -0500

    bug fix on last commit

--- aux/mailtools.py
+++ aux/mailtools.py
@@ -10,57 +10,57 @@ Toolbox for sending emails through SMTP
 """
 
 def send_error(error, email, lfil=None):
-	sender = 'noreply@kirschner.med.harvard.edu'
-	receivers = email
+    sender = 'noreply@kirschner.med.harvard.edu'
+    receivers = email
 
-	message = MIMEMultipart(
-		From="CRA Server <noreply@kirschner.med.harvard.edu>",
-	)
+    message = MIMEMultipart(
+        From="CRA Server <noreply@kirschner.med.harvard.edu>",
+    )
 
-	message['Subject'] = "CRA SERVER ERROR!!!"
+    message['Subject'] = "CRA SERVER ERROR!!!"
 
-	body = MIMEText(error, 'html')
-	message.attach(body)
+    body = MIMEText(error, 'html')
+    message.attach(body)
 
-	#make sure email can be send, and send
-	try:
-		smtpObj = smtplib.SMTP('localhost')
-		smtpObj.sendmail(sender, receivers, message.as_string())
-		print "Successfully sent email"
-	except smtplib.SMTPException:
-		if lfil is not None:
-			logtools.add_fatal_error(lfil, "UNABLE TO SEND EMAIL!!!")
-		print "Error: unable to send email"
+    #make sure email can be send, and send
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print "Successfully sent email"
+    except smtplib.SMTPException:
+        if lfil is not None:
+            logtools.add_fatal_error(lfil, "UNABLE TO SEND EMAIL!!!")
+        print "Error: unable to send email"
 
 
 def send_email(info, email, files, lfil=None, sub="CRA Results"):
-	sender = 'noreply@kirschner.med.harvard.edu'
-	receivers = email
+    sender = 'noreply@kirschner.med.harvard.edu'
+    receivers = email
 
-	message = MIMEMultipart()
-	message['To'] = email
-	message['From'] = "CRA Server <noreply@kirschner.med.harvard.edu>"
-	message['Subject'] = sub
+    message = MIMEMultipart()
+    message['To'] = email
+    message['From'] = "CRA Server <noreply@kirschner.med.harvard.edu>"
+    message['Subject'] = sub
 
-	body = MIMEText(info, 'html')
-	message.attach(body)
+    body = MIMEText(info, 'html')
+    message.attach(body)
 
-	for f in files or []:
-		try:
-			with open(f, "rb") as fil:
-				attach_file = MIMEApplication(fil.read())
-		except IOError:
-			open(f, "w").close()
-			with open(f, "rb") as fil:
-				attach_file = MIMEApplication(fil.read())
-		attach_file.add_header('Content-Disposition', 'attachment', filename=f.split('uploaded_fasta/')[1])
-		message.attach(attach_file)
+    for f in files or []:
+        try:
+            with open(f, "rb") as fil:
+                attach_file = MIMEApplication(fil.read())
+        except IOError:
+            open(f, "w").close()
+            with open(f, "rb") as fil:
+                attach_file = MIMEApplication(fil.read())
+        attach_file.add_header('Content-Disposition', 'attachment', filename=f)
+        message.attach(attach_file)
 
-	try:
-		smtpObj = smtplib.SMTP('localhost')
-		smtpObj.sendmail(sender, receivers, message.as_string())
-		print "Successfully sent email"
-	except smtplib.SMTPException:
-		if lfil is not None:
-			logtools.add_fatal_error(lfil, "UNABLE TO SEND EMAIL!!!")
-		print "Error: unable to send email"
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print "Successfully sent email"
+    except smtplib.SMTPException:
+        if lfil is not None:
+            logtools.add_fatal_error(lfil, "UNABLE TO SEND EMAIL!!!")
+        print "Error: unable to send email"
