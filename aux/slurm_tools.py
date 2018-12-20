c6f95aeab95a0b82e7c21bae08e31db1ee703873
Jon Patsenker <jonathan@patsenker.com>
2018-12-19 21:24:14 -0500

    bug fix on last commit

--- aux/slurm_tools.py
+++ aux/slurm_tools.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def srun(job_string, error, output, queue, timelim, wait):
+def srun(command, error, output, queue, timelim, wait):
     full_command = "slurm -p " + queue + " -t " + str(timelim) + " -o " + output + " -e " + error + " " + command
     print full_command
     proc = 'lol'
