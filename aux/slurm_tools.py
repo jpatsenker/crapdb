d9f615f5cd1ab592318c2fcd0b424063494a5885
Jon Patsenker <jonathan@patsenker.com>
2018-12-19 20:11:53 -0500

    adding slurm support

--- aux/slurm_tools.py
+++ aux/slurm_tools.py
@@ -3,7 +3,9 @@ import subprocess
 
 def srun(job_string, error, out, queue, timelim, wait):
     full_command = "slurm -p " + queue + " -t " + str(timelim) + " -o " + bsub_output + " -e " + bsub_error + " " + command
-    proc = subprocess.Popen([full_command])
+    print full_command
+    proc = 'lol'
+    #proc = subprocess.Popen([full_command])
     if wait:
         proc.wait()
     return proc
