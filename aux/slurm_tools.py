12e113b53f18b5db5f4a413f0f75065bb23c70a5
Jon Patsenker <jonathan@patsenker.com>
2018-12-21 17:28:59 -0500

    testing

--- aux/slurm_tools.py
+++ aux/slurm_tools.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def srun(command, error, output, queue, timelim, wait):
-    full_command = ['/usr/bin/srun', '--partition='+str(queue), '--time='+str(timelim), '--output='+str(output), '--error='+str(error), "`"]
+    full_command = ['/usr/bin/srun', '--partition='+str(queue), '--time='+str(timelim), '--output='+str(output), '--error='+str(error)]
     full_command.extend(command.split())
     full_command.append("`")
     full_command_str = " ".join(full_command)
