1e768044e49a3bfcfaa5b45278045b2558fe1c39
Jon Patsenker <jonathan@patsenker.com>
2018-12-20 19:42:33 -0500

    testing

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-p = subprocess.Popen(['srun -p short -t 1 -o stdout.txt -e stderr.txt "echo \'Hello World\'"'])
+p = subprocess.Popen(['srun -p short -t 1 "echo \'Hello World\'"'])
 p.wait()
 print "Done"
 
