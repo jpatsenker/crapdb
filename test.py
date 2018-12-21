fd0c21615b14503c1ecf9aa1d42f4ff5011ef2a1
Jon Patsenker <jonathan@patsenker.com>
2018-12-20 22:58:06 -0500

    testing

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 #p = subprocess.Popen(['whoami'])
-p = subprocess.Popen(['srun -p short -t 1 python test_slurm.py'])
+p = subprocess.Popen(['srun','-p short -t 1 python test_slurm.py'])
 p.wait()
 print "Done"
 
