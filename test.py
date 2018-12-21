19c832afa05843f11b5675f015ff9c38e143b836
Jon Patsenker <jonathan@patsenker.com>
2018-12-20 22:13:21 -0500

    testing

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
 
-p = subprocess.Popen(['sudo -u kirschner "srun -p short -t 1 python slurm_test.py"'])
+#p = subprocess.Popen(['sudo -s -u kirschner srun -p short -t 1 python test_slurm.py'])
+p = subprocess.Popen(['srun -p short -t 1 -uid=kirschner python test_slurm.py'])
 p.wait()
 print "Done"
 
