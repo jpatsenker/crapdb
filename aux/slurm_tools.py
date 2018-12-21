9e8a75c060f6518f84cfd3ba7be4c79ce53d7c1a
Jon Patsenker <jonathan@patsenker.com>
2018-12-21 14:12:07 -0500

    testing

--- aux/slurm_tools.py
+++ aux/slurm_tools.py
@@ -2,7 +2,8 @@ import subprocess
 
 
 def srun(command, error, output, queue, timelim, wait):
-    full_command = ['/usr/bin/srun', '--partition='+str(queue), '--time='+str(timelim), '--output='+str(output), '--error='+str(error), command]
+    full_command = ['/usr/bin/srun', '--partition='+str(queue), '--time='+str(timelim), '--output='+str(output), '--error='+str(error)]
+    full_command.extend(command.split())
     print full_command
     proc = subprocess.Popen(full_command)
     if wait:
