22c361bf7981722f80cedd26c0b83f079972ac49
Jon Patsenker <jonathan@patsenker.com>
2018-12-21 13:58:03 -0500

    testing

---
+++ file_paths.py
@@ -0,0 +1,10 @@
+#INTERPRETERS/COMPILERS
+PYTHON_PATH = '/usr/bin/python'
+PERL_PATH = '/usr/bin/perl'
+
+
+
+#RELEVANT SCRIPTS
+FASTACHECKER_PATH = "/www/kirschner.med.harvard.edu/docroot/genomes/code/fasta_checker_for_crap.pl"
+CDHIT_PATH = "FIND"
+ZEROJ_PATH = "/www/kirschner.med.harvard.edu/docroot/genomes/code/0j/0j.py"
