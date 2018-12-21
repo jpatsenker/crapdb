f9c4024704bf0142d05659e17bdcb168661b4c79
Jon Patsenker <jonathan@patsenker.com>
2018-12-20 22:09:11 -0500

    testing

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-p = subprocess.Popen(['sudo -u kirschner srun -p short -t 1 -o stdout.txt -e stderr.txt python slurm_test.py'])
+p = subprocess.Popen(['sudo -u kirschner "srun -p short -t 1 python slurm_test.py"'])
 p.wait()
 print "Done"
 
