0795a03616da1c18c7c8acb09db0648bddaf1e48
Jon Patsenker <jonathan@patsenker.com>
2018-12-26 13:40:22 -0500

    ok

--- results.php
+++ results.php
@@ -22,14 +22,44 @@
     $dComp = $_POST['dcomp'];
     $dRed = $_POST['dred'];
     $dLen = $_POST['dlen'];
-    $dFis = $_POST['dfis'];
+    #$dFis = $_POST['dfis'];
     $zj = $_POST['zj'];
     $xs = $_POST['xs'];
     $ms = $_POST['ms'];
     #$fft = $_POST['fft'];
     #$ffl = $_POST['ffl'];
     #$rg = $_POST['refgm'];
+    if ($dLen == "on"){
+        $dLen = " -nolen ";
+    }else{
+        $dLen = "";
+    }
+    if ($dRed == "on"){
+        $dRed = " -nored ";
+    }else{
+        $dRed = "";
+    }
+    if ($dComp == "on"){
+        $dComp = " -nocomp ";
+    }else{
+        $dComp = "";
+    }
+    #if ($dFis == "on"){
+    #    $dFis = " -nofis ";
+    #}else{
+    #    $dFis = "";
+    #}
+    if ($ms == "on"){
+        $ms = " -ms ";
+    }else{
+        $ms = "";
+    }
 
+    echo $email;
+    echo $dComp;
+    echo $xs;
+    echo $ms;
+    echo $max;
 ?>
 
     </BODY>
