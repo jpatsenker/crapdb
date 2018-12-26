ecfe3e85ecdb1747b6c5e1128bd3b9ebd14fc9dd
Jon Patsenker <jonathan@patsenker.com>
2018-12-26 13:18:46 -0500

    ok

--- results.php
+++ results.php
@@ -30,66 +30,63 @@
 
     echo '<table style="margin:0 auto;"><tr><td>';
     #echo '<div>';
-    if(!$_POST['email']){
-        echo '<div class="notout">';
-        echo '<p>  Please fill in all fields </p>';
+
+    #get information
+    $email = $_POST['email'];
+    $ct = $_POST['ct'];
+    $cl = $_POST['cl'];
+    $min = $_POST['min'];
+    $max = $_POST['max'];
+    $dComp = $_POST['dcomp'];
+    $dRed = $_POST['dred'];
+    $dLen = $_POST['dlen'];
+    $dFis = $_POST['dfis'];
+    $zj = $_POST['zj'];
+    $xs = $_POST['xs'];
+    $ms = $_POST['ms'];
+    #$fft = $_POST['fft'];
+    #$ffl = $_POST['ffl'];
+    #$rg = $_POST['refgm'];
+
+    if ($dLen == "on"){
+        $dLen = " -nolen ";
     }else{
-        #get information
-        $email = $_POST['email'];
-        $ct = $_POST['ct'];
-        $cl = $_POST['cl'];
-        $min = $_POST['min'];
-        $max = $_POST['max'];
-        $dComp = $_POST['dcomp'];
-        $dRed = $_POST['dred'];
-        $dLen = $_POST['dlen'];
-        $dFis = $_POST['dfis'];
-        $zj = $_POST['zj'];
-        $xs = $_POST['xs'];
-        $ms = $_POST['ms'];
-        #$fft = $_POST['fft'];
-        #$ffl = $_POST['ffl'];
-        #$rg = $_POST['refgm'];
-        
-        if ($dLen == "on"){
-            $dLen = " -nolen ";
-        }else{
-            $dLen = "";
-        }
-        if ($dRed == "on"){
-            $dRed = " -nored ";
-        }else{
-            $dRed = "";
-        }
-        if ($dComp == "on"){
-            $dComp = " -nocomp ";
-        }else{
-            $dComp = "";
-        }
+        $dLen = "";
+    }
+    if ($dRed == "on"){
+        $dRed = " -nored ";
+    }else{
+        $dRed = "";
+    }
+    if ($dComp == "on"){
+        $dComp = " -nocomp ";
+    }else{
+        $dComp = "";
+    }
 //         if ($dFis == "on"){
 //             $dFis = " -nofis ";
 //         }else{
 //             $dFis = "";
 //         }
-        if ($ms == "on"){
-            $ms = " -ms ";
-        }else{
-            $ms = "";
-        }
-        $target_dir = "uploaded_fasta/";
-        $next_id = get_next_id();
-        $fname = $_FILES['fastaseq']['name'];
-        $target_fname = substr($fname,0,strpos($fname,'.')) . '_' . $next_id;
-        $target_file = $target_dir . $target_fname;
-        
-        
-        
-        #move file into uploaded folder
-        if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
-            echo '<div class="notout">';
-            echo 'Error Uploading Input File <br>';
+    if ($ms == "on"){
+        $ms = " -ms ";
+    }else{
+        $ms = "";
+    }
+    $target_dir = "uploaded_fasta/";
+    $next_id = get_next_id();
+    $fname = $_FILES['fastaseq']['name'];
+    $target_fname = substr($fname,0,strpos($fname,'.')) . '_' . $next_id;
+    $target_file = $target_dir . $target_fname;
 
-        }else{
+
+
+    #move file into uploaded folder
+    if(!move_uploaded_file($_FILES['fastaseq']['tmp_name'], $target_file)){
+        echo '<div class="notout">';
+        echo 'Error Uploading Input File <br>';
+
+    }else{
 
 //             if($rg == "custom"){
 //                 $rfname = $_FILES['fastaseq']['name'];
@@ -105,35 +102,33 @@
 //             }
 
 
-            echo '<div class="outputs">';
+        echo '<div class="outputs">';
 
-            $headers = 'From: "CoreCop Pipeline" <noreply@kirschner.med.harvard.edu>\r\n';
-            $headers .= "MIME-Version: 1.0\r\n";
-            $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $headers = 'From: "CoreCop Pipeline" <noreply@kirschner.med.harvard.edu>\r\n';
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-            $fullpath = substr(getcwd(),strpos(getcwd(), "/www/") + 5) . "/logs/" . $target_fname . ".log";
-            $fullpath = str_replace("/docroot/", "/", $fullpath);
-            $sent = @mail($email, "CoreCop REQUEST SENT", "We are processing your file as: " . $target_file . " size: " . filesize($target_file) . ' bytes.<br> ' . $fullpath . '<br>', $headers);
-            if($sent){
-                echo "<p> Email Validated... </p>";
-            }else{
-                echo "<p> INVALID EMAIL! TERMINATING JOB! </p>";
-                echo '<form action="index.php"><input type="button" value="Back" onClick="history.go(-1);return true;"></form>';
-                echo "</div></td></tr></table>";
-                die();
-            }
-            
-            echo "<p> We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes </p>";
-            echo 'python run_cra_interface.py ' . $target_file . ' ' . $target_file . '.clean.fa ' . $target_file . '.messy.fa ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed .' > superlog 2>&1 &';
-            exec('python run_cra_interface.py ' . $target_file . ' ' . $target_file . '.clean.fa ' . $target_file . '.messy.fa ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed .' > superlog 2>&1 &');
-            
-            if(!$_POST['email']){
-                echo '<p> You will receive an email when the results are ready. </p>';
-            }
-            echo '<p><a href="logs/' . $target_fname . '.log"> Log file for job </a></p>';
-
+        $fullpath = substr(getcwd(),strpos(getcwd(), "/www/") + 5) . "/logs/" . $target_fname . ".log";
+        $fullpath = str_replace("/docroot/", "/", $fullpath);
+        $sent = @mail($email, "CoreCop REQUEST SENT", "We are processing your file as: " . $target_file . " size: " . filesize($target_file) . ' bytes.<br> ' . $fullpath . '<br>', $headers);
+        if($sent){
+            echo "<p> Email Validated... </p>";
+        }else{
+            echo "<p> INVALID EMAIL! TERMINATING JOB! </p>";
+            echo '<form action="index.php"><input type="button" value="Back" onClick="history.go(-1);return true;"></form>';
+            echo "</div></td></tr></table>";
+            die();
         }
-            
+
+        echo "<p> We are processing your file as: " . $target_file . " size: " . filesize($target_file) . " bytes </p>";
+        echo 'python run_cra_interface.py ' . $target_file . ' ' . $target_file . '.clean.fa ' . $target_file . '.messy.fa ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed .' > superlog 2>&1 &';
+        exec('python run_cra_interface.py ' . $target_file . ' ' . $target_file . '.clean.fa ' . $target_file . '.messy.fa ' . $email . ' -ct ' . $ct . ' -cl ' . $cl . ' -0j ' . $zj . ' -min ' . $min . ' -max ' . $max . $ms . ' -xs ' . $xs . $dComp . $dLen . $dRed .' > superlog 2>&1 &');
+
+        if(!$_POST['email']){
+            echo '<p> You will receive an email when the results are ready. </p>';
+        }
+        echo '<p><a href="logs/' . $target_fname . '.log"> Log file for job </a></p>';
+
     }
 
     echo '<form action="index.php"><input type="button" value="Back" onClick="history.go(-1);return true;"></form>';
