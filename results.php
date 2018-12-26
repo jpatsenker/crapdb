d0015b94be0ed0c105d55fe013feac4733caf4a1
Jon Patsenker <jonathan@patsenker.com>
2018-12-26 13:36:15 -0500

    ok

--- results.php
+++ results.php
@@ -11,6 +11,24 @@
 <?php
 
     parse_ini_file("php.ini");
+    echo '<table style="margin:0 auto;"><tr><td>';
+
+    #get information
+    $email = $_POST['email'];
+    $ct = $_POST['ct'];
+    $cl = $_POST['cl'];
+    $min = $_POST['min'];
+    $max = $_POST['max'];
+    $dComp = $_POST['dcomp'];
+    $dRed = $_POST['dred'];
+    $dLen = $_POST['dlen'];
+    $dFis = $_POST['dfis'];
+    $zj = $_POST['zj'];
+    $xs = $_POST['xs'];
+    $ms = $_POST['ms'];
+    #$fft = $_POST['fft'];
+    #$ffl = $_POST['ffl'];
+    #$rg = $_POST['refgm'];
 
 ?>
 
