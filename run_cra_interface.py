afdbb378fe7093cef8ae18ae40783caf5e83fc0a
Doug Feldmann <doug@feldmann.com>
2019-11-27 12:03:56 -0500

    Remove job submission code from run_cra_interface.py and import from aux.jobs instead

--- run_cra_interface.py
+++ run_cra_interface.py
@@ -1,6 +1,7 @@
 print "running..."
 import sys
 import os
+import aux.jobs
 """
 #Interface for running run_cra from php
 """
@@ -11,7 +12,7 @@ import os
 #sys.path.append('/n/www/kirschner.med.harvard.edu/docroot/corecop/aux')
 
 
-import subprocess
+#import subprocess
 
 
 """
@@ -24,38 +25,6 @@ CONTACT_EMAIL = "<contact-email>"
 def add_line_to_log(log_file, line):
     with open(log_file, "a") as lfil:
         lfil.write(str(line) + "\n")
-
-
-def srun(command, error, output, queue, timelim, wait):
-    full_command = ['/usr/bin/sbatch', '-D'+str(os.getcwd()), '--partition='+str(queue),
-                    '--time='+str(timelim), '--output='+str(output),
-                    '--error='+str(error), "--wrap='%s'" % command ]
-    full_command_str = " ".join(full_command)
-    print full_command_str
-    proc = subprocess.Popen(full_command)
-    #temporarily disabled
-#    if wait:
-#        proc.wait()
-    return proc
-
-
-
-class Job:
-    def __init__(self, job_string, lfil=None):
-        self.job_string = job_string
-        self.lfil = lfil
-
-    def run(self, error = 'error_test', output='output_test', queue='short', timelim = 60, wait = False, return_process = False):
-        if self.lfil is not None:
-            logtools.add_line_to_log(self.lfil, "<CMD:> [SRUN] " + self.job_string)
-
-        proc = srun(self.job_string, error, output, queue, timelim, wait)
-
-        if wait and self.lfil is not None:
-            logtools.add_line_to_log(self.lfil, "<CMD EXECUTED>")
-
-        if return_process:
-            return proc
 
 #print os.getcwd()
 #import aux
@@ -70,7 +39,7 @@ for a in sys.argv[1:]:
 
 print command_string + "\n"
 
-job = Job(command_string)
-job.run(wait=True, output="interface_test_out", error="interface_test_err")
+job = aux.jobs.Job(command_string)
+job.run(wait=False, output="interface_test_out", error="interface_test_err")
 
 print "done"
